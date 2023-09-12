db8967df63202a8397d3870673014958f28b975a
Domenico Natale <56569545+dnatale731@users.noreply.github.com>
2023-09-12 11:25:37 -0400

    Add files from Simulation

---
+++ New York Jobs CEO Council/task_2_code.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+
+class Invoice:
+    """Represents an invoice for a collection of services rendered to a recipient"""
+
+    def __init__(self,
+                 sender_name,
+                 recipient_name,
+                 sender_address,
+                 recipient_address,
+                 sender_email,
+                 recipient_email):
+        # externally determined variables
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
+        self.sender_address = sender_address
+        self.recipient_address = recipient_address
+        self.sender_email = sender_email
+        self.recipient_email = recipient_email
+
+        # internally determined variables
+        self.date = datetime.now()
+        self.cost = 0
+        self.items = []
+        self.comments = []
+
+    def add_item(self, name, price, tax):
+        # python makes working with trivial data-objects quite easy
+        item = {
+            "name": name,
+            "price": price,
+            "tax": tax
+        }
+
+        # hold on to the unmodified item for later, we'll do tax/discount calculations on an as-needed basis
+        self.items.append(item)
+
+    def add_comment(self, comment):
+        # hold on to the unmodified comment for later
+        self.comments.append(comment)
+
+    def calculate_total(self, discount):
+        # determine how much the invoice total should be by summing all individual item totals
+        total = 0
+        for item in self.items:
+            price = item["price"]
+            tax = item["tax"]
+            subtotal = price * (1 - discount)
+            total += subtotal * (1 + tax)
+        return total
+    
+    def print_comments(self):
+        for comment in self.comments:
+            print(comment)
+
+
+if __name__ == '__main__':
+    invoice = Invoice(
+        "Larry Jinkles",
+        "Tod Hooper",
+        "34 Windsor Ln.",
+        "14 Manslow road",
+        "lejank@billing.com",
+        "discreetclorinator@hotmail.com"
+    )
+
+    invoice.add_item("34 floor building", 3400, .1)
+    invoice.add_item("Equipment Rental", 1000, .1)
+    invoice.add_item("Fear Tax", 340, 0.0)
+    invoice.add_comment("Equipment will be rented until 9/16")
+    invoice.add_comment("Customer concerned by fear tax. Mission Accomplished!")
+    invoice_total = invoice.calculate_total(.2)
+    print(invoice_total)
+    invoice.print_comments()
